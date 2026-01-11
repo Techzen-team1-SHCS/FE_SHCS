ef203e5c6d639844760d4e9b2e6a1c5689fe9012
TranVi <vit76404@gmail.com>
2026-01-11 18:19:02 +0700

    finish chatAI

---
+++ src/components/ChatAI/ChatButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ChatButton = () => {
+  return (
+    <div>ChatButton</div>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,6 +28,7 @@ const Home = () => {
                 setIsAuthVisible(true);
             }} />}
             
+            
         </div>
     )
 }
