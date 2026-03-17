761de1903bec09ad52c1c01133052858168908d0
TranVi <vit76404@gmail.com>
2026-03-17 11:50:33 +0700

    fix

--- src/config/echo.js
+++ src/config/echo.js
@@ -6,15 +6,15 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: 'fm1tyjgkqjeyxj3yczff',
-    wsHost: 'be-shcs.onrender.com',
-    wsPort: 443,
-    wssPort: 443,
-    forceTLS: true,
+    wsHost: window.location.hostname,
+    wsPort: 8080,
+    wssPort: 8080,
+    forceTLS: false,
     enabledTransports: ['ws', 'wss'],
     auth: {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
     },
-    authEndpoint: `${import.meta.env.VITE_API_URL}/broadcasting/auth`,
+    authEndpoint: 'http://127.0.0.1:8000/broadcasting/auth',
 });
