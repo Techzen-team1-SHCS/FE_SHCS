db0f47ef8251dd42cffad5ac2993961615b9cc92
thientu3012 <nguyenththientu@gmail.com>
2026-03-20 11:43:40 +0700

    feat(hotel-manager): add authentication pages for login and register

---
+++ src/pages/HotelManager/Mock/AutMock.js
@@ -0,0 +1,9 @@
+export const mockLogin = (data) => {
+  console.log("Login:", data);
+  return { success: true };
+};
+
+export const mockRegister = (data) => {
+  console.log("Register:", data);
+  return { success: true };
+};
