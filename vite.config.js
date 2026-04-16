8e6709a9fe8d67ff27fd53998d92c1d9726d99ea
TranVi <vit76404@gmail.com>
2026-04-16 10:25:39 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/FE_SHCS/",
+  base: "./",
   plugins: [react()],
   server: {
     proxy: {
