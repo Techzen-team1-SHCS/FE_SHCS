0ad2d61c18147a03ff201c0635e76cd37c3649fb
thientu3012 <nguyenththientu@gmail.com>
2026-04-19 14:12:50 +0700

    refactor:fix

--- src/pages/Admin/Notification/components/NotificationItem.jsx
+++ src/pages/Admin/Notification/components/NotificationItem.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from '../Notification.module.css';
 import { NOTIFICATION_TYPES } from '../Constants/notificationConstants';
 import { formatTimeAgo } from '../helpers/formatTime';
